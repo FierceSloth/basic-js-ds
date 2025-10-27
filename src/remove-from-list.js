234255a4f424958071a0401a6cba732ec2771172
Dastan Hairushev <hairushevdastan@gmail.com>
2025-10-27 17:47:13 +0500

    feat: complete remove from list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,9 +21,26 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  let prev = null;
+  let current = l;
+  while (current) {
+    if (current.value === k) {
+      if (!prev) {
+        l = l.next;
+        current = l;
+        continue
+      }
+      prev.next = current.next;
+      current = current.next;
+      continue;
+    }
+
+    prev = current
+    current = current.next;
+  }
+
+  return l;
 }
 
 module.exports = {
